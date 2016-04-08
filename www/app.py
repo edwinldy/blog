b0fe18ef1003a4c6ff86ac95ef301acef1986a3b
edw1nl <edw1nl@outlook.com>
2016-04-08 17:01:50 +0800

    改为只返回html格式

--- www/app.py
+++ www/app.py
@@ -16,7 +16,7 @@ def get():
     r.encoding = 'utf-8'
     readable_article = Document(r.text).summary()
 #    markdownify_article = html2text.html2text(readable_article)
-    return html2text.html2text(readable_article)
+    return readable_article
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000)
